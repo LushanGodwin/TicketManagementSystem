0d75952accdd34a158e0a358dffdc7a7bd02146f
Your Name <you@example.com>
2024-06-25 22:28:11 +0530

    add service-reg , api-gateway & config-server

---
+++ service-reg/src/main/java/lk/ijse/servicereg/ServiceRegApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.servicereg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRegApplication.class, args);
+    }
+
+}
