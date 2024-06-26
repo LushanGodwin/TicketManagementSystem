2cf82a67e682741a98b1f9d2542c9979c9cf9108
Your Name <you@example.com>
2024-06-26 17:04:25 +0530

    add vehical-service

---
+++ vehical-service/src/main/java/lk/ijse/vehicalservice/VehicalServiceApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.vehicalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicalServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicalServiceApplication.class, args);
+    }
+
+}
---
+++ vehical-service/src/test/java/lk/ijse/vehicalservice/VehicalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.vehicalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicalServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
